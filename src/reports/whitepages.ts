db8c7f0e69e9f07633cc6b9f227e0397130c3c2e
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-09-04 17:23:24 +0200

    update whitepages with unique ids

--- src/reports/whitepages.ts
+++ src/reports/whitepages.ts
@@ -3,260 +3,260 @@
 
 export const WHITEPAGES = [
   {
-    address: "9763a662ac509b8e6aae78aa628cbf46569a46e6e94b080c35e44d28368ee0ea",
-    handle: "zoz"
+    "address": "79784b5981cc5407ba3bf9f7526f3f54",
+    "handle": "_easy123"
   },
   {
-    address: "9de57cf5f154889bd4bdfb286fe1aedc7f58e7640f14dbe5f12cfff43255be48",
-    handle: "zeroooooooool"
+    "address": "6ec6056a12fba4d82aa17f0a8005b5b8f044aef79519a60bf1769949a21caed",
+    "handle": "0xslipk.1"
   },
   {
-    address: "55b6ba3408f670418371f500b9f431dc0e867f09974812b32c6d3a614d35cca6",
-    handle: "wuya51"
+    "address": "c80696fa9d3d161261246d3fdbfd982402856dfe22ffc571b80c3acc67c2bda4",
+    "handle": "0xslipk.2"
   },
   {
-    address: "be3a60be9e90510b821ad8abab51d0c11720c4fa7e76d6d06031a836627e5d6e",
-    handle: "wiri"
+    "address": "d19a9e5149ab0038f3563caf6b3e7063dca5bfe9dc37cb7f7194806ceaa4f5c4",
+    "handle": "0xslipk.3"
   },
   {
-    address: "193960de6f89b296f6c25046a97ce5e87405ac04d2d930c9ab57159076077d34",
-    handle: "vistli"
+    "address": "aa7b724a51a8bd8e55208fef969209b3",
+    "handle": "0xslipk.4"
   },
   {
-    address: "949ea6affb7a4b43f157158319b5d51c",
-    handle: "veight"
+    "address": "ca6bf512e1ca96ed2a6ef5b74bb8ba5daca689e8b314c4414440ebeb5b82fdf2",
+    "handle": "aaronwhite"
   },
   {
-    address: "06ec6056a12fba4d82aa17f0a8005b5b8f044aef79519a60bf1769949a21caed",
-    handle: "0xslipk"
+    "address": "56641e58aba97fa6b7ea833f83444392",
+    "handle": "abed"
   },
   {
-    address: "86554d60d94378e32ee3ec053d8e32c3f1b37725a73a95f084381edfc1e76354",
-    handle: "m0ot"
+    "address": "32f24e0488a4e189d38fccd1f2a94b53",
+    "handle": "alanyoon.1"
   },
   {
-    address: "c80696fa9d3d161261246d3fdbfd982402856dfe22ffc571b80c3acc67c2bda4",
-    handle: "0xslipk"
+    "address": "ab8071fddaacc1a48ca6901d87ad3d181f8bd344bc8defee1bc353cb26f2cefe",
+    "handle": "alanyoon.2"
   },
   {
-    address: "d19a9e5149ab0038f3563caf6b3e7063dca5bfe9dc37cb7f7194806ceaa4f5c4",
-    handle: "0xslipk"
+    "address": "6bb062c9611aa91a3300eef90b9937863c5fd1781c0eebac6abfc5ae1e44118f",
+    "handle": "allen010101"
   },
   {
-    address: "e3d049469498319fe30b9aa3ac95d72be4c0f0eb8b436866e2e0b33334fc082a",
-    handle: "blackgrouse"
+    "address": "4edef1a648ce28eaa71e3d6eb205a0f9c668bb7f025e6d3de4e133657fdd093a",
+    "handle": "andersph"
   },
   {
-    address: "2023c65e5323feb2671eb690d5649ae3",
-    handle: "theoneid"
+    "address": "8186f4d8fdd09dfe02963b0b4c385105",
+    "handle": "ashiix"
   },
   {
-    address: "25c39d475e12cd20623b75d33046baa442b8115c80fea816bf0198954ae9e341",
-    handle: "texaseduc8r"
+    "address": "9499c45e34ef05a9afd295779d989cbafaf973dbcdd35a9ae6be5fa8afa9861f",
+    "handle": "barmaley"
   },
   {
-    address: "f48513d19e39488b20ff63a798affa544d45db9aca5e544fdd2e8b6e4a4c017e",
-    handle: "slappymcslapper"
+    "address": "9a01290c3ab972597a71194428318cd87361fe41ce9a451eebfe8d3748377a2e",
+    "handle": "coin1111"
   },
   {
-    address: "9a710919b1a1e67eda335269c0085c91",
-    handle: "sirouk"
+    "address": "95c872c5519a0e165886b4e752f23b545805a218e50f0e846f5976ea3b026ae5",
+    "handle": "bbk8530.1"
   },
   {
-    address: "4d250de8388d8232f702ffc6de74df626325cbd24ad416350ab2a4e30b2c92c6",
-    handle: "saturn"
+    "address": "d578395a5a130fcd0ff13ed6a1c5af37",
+    "handle": "bbk8530.2"
   },
   {
-    address: "b5b5ba58b8e9916fe449d1f989383834",
-    handle: "ricoflan"
+    "address": "754903132f75c0b1f5e5be29fb0ca6cd",
+    "handle": "bethose"
   },
   {
-    address: "6122b508960bbdbbf28f38bc035c393ecf7cff54c3ce8282c735940eedd807a",
-    handle: "qusuy"
+    "address": "b32e1c7b70c711d7992fee1219ecaa50dfd2cfe9283b68e2ae6aff1c990f80dd",
+    "handle": "blackgrouse.1"
   },
   {
-    address: "9909f6edb2a0684adecd7b448b989e4f",
-    handle: "pithecus"
+    "address": "e3d049469498319fe30b9aa3ac95d72be4c0f0eb8b436866e2e0b33334fc082a",
+    "handle": "blackgrouse.2"
   },
   {
-    address: "b3848a1353f194692551661fe43933b4af80f92e75e9ecc2d2b08e4ca970e0d8",
-    handle: "pedr00"
+    "address": "f2103cc8b36205aed79c8e5a441fc4e75af07d5e33325491f08a98f604b5fd78",
+    "handle": "blackgrouse.3"
   },
   {
-    address: "c88f9adcb4c0678b9475e79355c53bb4",
-    handle: "nuke"
+    "address": "aaa0810d589f9681f0b793d567d3888ae38e7db2e842d29a2a9b8f59135e529a",
+    "handle": "borkd"
   },
   {
-    address: "e77ddb76c9afcb3d5511e46cbc89023d",
-    handle: "nourspace"
+    "address": "d1cc0e59af9eb9d0ec6c592d42270de8a415633f74e1429b031b29afde1e40a8",
+    "handle": "brakod"
   },
   {
-    address: "d785eb2412263beb0dbbb6468f35a1a",
-    handle: "ncontrol88"
+    "address": "13a65b0c9500848a6e8cc9a8c45e2c0750f6860fe9aad7e956e27de10956dddd",
+    "handle": "c64michael"
   },
   {
-    address: "a35ae41a5f1701fcb67f4e4cad59d259",
-    handle: "ncontrol88"
+    "address": "b749845cf5931a9237ad672fbf501530d457de2f241eb2acfb789daa4f035f68",
+    "handle": "chrisb_2002"
   },
   {
-    address: "e202aa9b2cfdb96627531af17832f2ca",
-    handle: "ncontrol88"
+    "address": "1fffe941a07d96a181a30934c9dbece13f1f6ae7df3af40203ecd31dba80ecd9",
+    "handle": "dboreham"
   },
   {
-    address: "a737f0a84b15f05857d531068089cbb3c073f9f99361f829a76cc6879797d8cb",
-    handle: "mr volt"
+    "address": "665ec87027a8022b8b5470239c898afe7d45e69712a43975f3bc1fcd1c645863",
+    "handle": "dlqsoftware"
   },
   {
-    address: "71e5be86b41516743ac8594bc90ee2fc",
-    handle: "mrrobot"
+    "address": "b8ba6c084fa504add5f23ae51e41f23d",
+    "handle": "dmski1"
   },
   {
-    address: "abf767f204672cba607bc730acbc982872437d9d26c836a36f6c4f976bc59ca9",
-    handle: "wuya51"
+    "address": "78233906ce0643d1852a7d4899a7666f",
+    "handle": "east agile"
   },
   {
-    address: "52d79701968e2ab8d34ddd77ad11e87f",
-    handle: "luckyduder"
+    "address": "6ed9621b48683455219bd8d9db54210304ac1a5fd7c578d358e8e3202d82803a",
+    "handle": "gabyy_91"
   },
   {
-    address: "fdcf0ef094b962fb35997f4e2bcd4f27533b567bf8518f5b4f9400f47742be9b",
-    handle: "lemonium"
+    "address": "77f2650cd59335a8b03f875b43376f26",
+    "handle": "hemulin"
   },
   {
-    address: "744185aebd902421992147559f35eb937058ac74116fd6d1d6bc93ab95805aea",
-    handle: "kryptikpanda"
+    "address": "b295da228c6808199d92dc813975755c",
+    "handle": "jack lin"
   },
   {
-    address: "2000fa5e04f476a50692ca1bb28e1629a4a642cf988cff27635550d85f9577a1",
-    handle: "kd"
+    "address": "2fa553d9ab2d5e0bf39bf9147b61a5d0fd78ebe7256ace2c9470020fbbea95a0",
+    "handle": "johnpaul.1"
   },
   {
-    address: "4d3dd09c797f5962bbd0afbc19e22b9",
-    handle: "kamp"
+    "address": "b1ebce2a604d0d277ff1b5f9ea7a4e8a8370c8f487edea5d7df85448b828cfd6",
+    "handle": "johnpaul.2"
   },
   {
-    address: "cf18bf38fe71080c3c199868ed167693",
-    handle: "kalvinen"
+    "address": "16b3da73e53d17bfeb6b542f1ac33444",
+    "handle": "judasdaygame"
   },
   {
-    address: "16b3da73e53d17bfeb6b542f1ac33444",
-    handle: "judasdaygame"
+    "address": "cf18bf38fe71080c3c199868ed167693",
+    "handle": "kalvinen"
   },
   {
-    address: "2fa553d9ab2d5e0bf39bf9147b61a5d0fd78ebe7256ace2c9470020fbbea95a0",
-    handle: "johnpaul"
+    "address": "4d3dd09c797f5962bbd0afbc19e22b9",
+    "handle": "kamp"
   },
   {
-    address: "b1ebce2a604d0d277ff1b5f9ea7a4e8a8370c8f487edea5d7df85448b828cfd6",
-    handle: "johnpaul"
+    "address": "2000fa5e04f476a50692ca1bb28e1629a4a642cf988cff27635550d85f9577a1",
+    "handle": "kd"
   },
   {
-    address: "b295da228c6808199d92dc813975755c",
-    handle: "jack lin"
+    "address": "744185aebd902421992147559f35eb937058ac74116fd6d1d6bc93ab95805aea",
+    "handle": "kryptikpanda"
   },
   {
-    address: "77f2650cd59335a8b03f875b43376f26",
-    handle: "hemulin"
+    "address": "fdcf0ef094b962fb35997f4e2bcd4f27533b567bf8518f5b4f9400f47742be9b",
+    "handle": "lemonium9962"
   },
   {
-    address: "6ed9621b48683455219bd8d9db54210304ac1a5fd7c578d358e8e3202d82803a",
-    handle: "gabyy_91"
+    "address": "52d79701968e2ab8d34ddd77ad11e87f",
+    "handle": "luckyduder"
   },
   {
-    address: "78233906ce0643d1852a7d4899a7666f",
-    handle: "east agile"
+    "address": "86554d60d94378e32ee3ec053d8e32c3f1b37725a73a95f084381edfc1e76354",
+    "handle": "m0ot"
   },
   {
-    address: "b8ba6c084fa504add5f23ae51e41f23d",
-    handle: "dmski1"
+    "address": "71e5be86b41516743ac8594bc90ee2fc",
+    "handle": "mrrobot"
   },
   {
-    address: "665ec87027a8022b8b5470239c898afe7d45e69712a43975f3bc1fcd1c645863",
-    handle: "dlqsoftware"
+    "address": "a737f0a84b15f05857d531068089cbb3c073f9f99361f829a76cc6879797d8cb",
+    "handle": "mrvolt"
   },
   {
-    address: "1fffe941a07d96a181a30934c9dbece13f1f6ae7df3af40203ecd31dba80ecd9",
-    handle: "dboreham"
+    "address": "a35ae41a5f1701fcb67f4e4cad59d259",
+    "handle": "ncontrol88.1"
   },
   {
-    address: "b749845cf5931a9237ad672fbf501530d457de2f241eb2acfb789daa4f035f68",
-    handle: "chrisb_2002"
+    "address": "d785eb2412263beb0dbbb6468f35a1a",
+    "handle": "ncontrol88.2"
   },
   {
-    address: "13a65b0c9500848a6e8cc9a8c45e2c0750f6860fe9aad7e956e27de10956dddd",
-    handle: "c64michael"
+    "address": "e202aa9b2cfdb96627531af17832f2ca",
+    "handle": "ncontrol88.3"
   },
   {
-    address: "d1cc0e59af9eb9d0ec6c592d42270de8a415633f74e1429b031b29afde1e40a8",
-    handle: "brakod"
+    "address": "e77ddb76c9afcb3d5511e46cbc89023d",
+    "handle": "nourspace"
   },
   {
-    address: "aaa0810d589f9681f0b793d567d3888ae38e7db2e842d29a2a9b8f59135e529a",
-    handle: "borkd"
+    "address": "c88f9adcb4c0678b9475e79355c53bb4",
+    "handle": "nuke"
   },
   {
-    address: "b32e1c7b70c711d7992fee1219ecaa50dfd2cfe9283b68e2ae6aff1c990f80dd",
-    handle: "blackgrouse"
+    "address": "b3848a1353f194692551661fe43933b4af80f92e75e9ecc2d2b08e4ca970e0d8",
+    "handle": "pedr00"
   },
   {
-    address: "f2103cc8b36205aed79c8e5a441fc4e75af07d5e33325491f08a98f604b5fd78",
-    handle: "blackgrouse"
+    "address": "9909f6edb2a0684adecd7b448b989e4f",
+    "handle": "pithecus"
   },
   {
-    address: "754903132f75c0b1f5e5be29fb0ca6cd",
-    handle: "bethose"
+    "address": "6122b508960bbdbbf28f38bc035c393ecf7cff54c3ce8282c735940eedd807a",
+    "handle": "qusuy"
   },
   {
-    address: "95c872c5519a0e165886b4e752f23b545805a218e50f0e846f5976ea3b026ae5",
-    handle: "bbk8530"
+    "address": "b5b5ba58b8e9916fe449d1f989383834",
+    "handle": "ricoflan"
   },
   {
-    address: "d578395a5a130fcd0ff13ed6a1c5af37",
-    handle: "bbk8530"
+    "address": "4d250de8388d8232f702ffc6de74df626325cbd24ad416350ab2a4e30b2c92c6",
+    "handle": "saturn"
   },
   {
-    address: "9a01290c3ab972597a71194428318cd87361fe41ce9a451eebfe8d3748377a2e",
-    handle: "coin111"
+    "address": "9a710919b1a1e67eda335269c0085c91",
+    "handle": "sirouk"
   },
   {
-    address: "9499c45e34ef05a9afd295779d989cbafaf973dbcdd35a9ae6be5fa8afa9861f",
-    handle: "barmaley1"
+    "address": "f48513d19e39488b20ff63a798affa544d45db9aca5e544fdd2e8b6e4a4c017e",
+    "handle": "slappymcslapper"
   },
   {
-    address: "8186f4d8fdd09dfe02963b0b4c385105",
-    handle: "ashiix"
+    "address": "25c39d475e12cd20623b75d33046baa442b8115c80fea816bf0198954ae9e341",
+    "handle": "texaseduc8r"
   },
   {
-    address: "4edef1a648ce28eaa71e3d6eb205a0f9c668bb7f025e6d3de4e133657fdd093a",
-    handle: "andersph"
+    "address": "2023c65e5323feb2671eb690d5649ae3",
+    "handle": "theoneid"
   },
   {
-    address: "6bb062c9611aa91a3300eef90b9937863c5fd1781c0eebac6abfc5ae1e44118f",
-    handle: "allen010101"
+    "address": "949ea6affb7a4b43f157158319b5d51c",
+    "handle": "veight"
   },
   {
-    address: "32f24e0488a4e189d38fccd1f2a94b53",
-    handle: "alanyoon"
+    "address": "193960de6f89b296f6c25046a97ce5e87405ac04d2d930c9ab57159076077d34",
+    "handle": "vistli"
   },
   {
-    address: "ab8071fddaacc1a48ca6901d87ad3d181f8bd344bc8defee1bc353cb26f2cefe",
-    handle: "alanyoon"
+    "address": "be3a60be9e90510b821ad8abab51d0c11720c4fa7e76d6d06031a836627e5d6e",
+    "handle": "wiri"
   },
   {
-    address: "56641e58aba97fa6b7ea833f83444392",
-    handle: "abed"
+    "address": "abf767f204672cba607bc730acbc982872437d9d26c836a36f6c4f976bc59ca9",
+    "handle": "wuya51.1"
   },
   {
-    address: "ca6bf512e1ca96ed2a6ef5b74bb8ba5daca689e8b314c4414440ebeb5b82fdf2",
-    handle: "aaronwhite"
+    "address": "55b6ba3408f670418371f500b9f431dc0e867f09974812b32c6d3a614d35cca6",
+    "handle": "wuya51.2"
   },
   {
-    address: "aa7b724a51a8bd8e55208fef969209b3",
-    handle: "0xslipk"
+    "address": "9de57cf5f154889bd4bdfb286fe1aedc7f58e7640f14dbe5f12cfff43255be48",
+    "handle": "zeroooooooool"
   },
   {
-    address: "79784b5981cc5407ba3bf9f7526f3f54",
-    handle: "_easy123"
+    "address": "9763a662ac509b8e6aae78aa628cbf46569a46e6e94b080c35e44d28368ee0ea",
+    "handle": "zoz"
   }
 ]
 
